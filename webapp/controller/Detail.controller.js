42e2a4b93e0b1c8cfcbdd64ea8e5be9e35959952
entw_dplehn <Detlef.Plehn@ede.de>
2017-06-28 14:18:10 +0200

    BAsecontroller

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,30 +1,68 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function(Controller, History) {
+    "sapui5/demo/mvcapp/controller/BaseController"
+], function (BaseController) {
     "use strict";
-    return Controller.extend("sapui5.demo.mvcapp.controller.Detail", {
-        onInit: function() {
-            this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            this._oRouter.getRoute("detail").attachPatternMatched(this._onDetailMatched, this);
+    return BaseController.extend("sapui5.demo.mvcapp.controller.Detail", {
+        /* ======================================================= */
+        /* lifecycle methods */
+        /* ======================================================= */
+        /**
+         * Called when the worklist controller is instantiated.
+         * @public
+         */
+        onInit: function () {
+            this.getRouter().getRoute("detail").attachPatternMatched(this
+                ._onObjectMatched, this);
+        },
+        /* ======================================================= */
+        /* event handlers */
+        /* ======================================================= */
+        /**
+         * Navigates back to the Master
+         * @function
+         */
+        onNavPress: function () {
+            this.myNavBack("master");
+        },
+        /* ======================================================= */
+        /* formatters */
+        /* ======================================================= */
+        /**
+         * Formats a given string to uppercase.
+         *
+         * @function
+         * @param {string} sName string to be formatted
+         * @public
+         */
+        toUpperCase: function (sName) {
+            return sName && sName.toUpperCase();
         },
 
-        _onDetailMatched: function(oEvent) {
-            var sObjectPath =
-                "/Suppliers/" + oEvent.getParameter("arguments").ID;
+        /**
+         * Binds the view to the object path.
+
+         *
+         * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event
+         * in route 'object'
+         * @private
+         */
+        _onObjectMatched: function (oEvent) {
+            var sObjectPath = "/Suppliers/" + oEvent.getParameter("arguments").ID;
+            this._bindView(sObjectPath);
+        },
+        /**
+         * Binds the view to the object path.
+         *
+         * @function
+         * @param {string} sObjectPath path to the object to be bound
+         * @private
+         */
+        _bindView: function (sObjectPath) {
             var oView = this.getView();
             oView.bindElement(sObjectPath);
-        },
-
-        onNavPress: function() {
-            var oHistory = History.getInstance();
-            var sPreviousHash = oHistory.getPreviousHash();
-            if (sPreviousHash !== undefined) {
-            // history contains a previous entry
-                window.history.go(-1);
-            } else {
-                this._oRouter.navTo("master");
-            }
         }
     });
 });
+
+
